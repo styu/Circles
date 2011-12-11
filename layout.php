f97630513e73ddc065cb492922a5d7f6290e779e
Victor Hung <victorhung92@gmail.com>
2011-12-11 01:54:23 -0500

    Basic Layout Established

--- layout.php
+++ layout.php
@@ -1,1 +1,41 @@
-Hi, Layout
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+   <head>
+      	<title>Circles - Interactive Poem</title>
+      
+      	<!-- stylesheets --> 
+      	<link href="reset.css" rel="stylesheet" type="text/css"/>
+      	<link href="style.css" rel="stylesheet" type="text/css"/>
+    
+		<!-- fonts --> 
+    	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,800" rel="stylesheet" type="text/css">
+    	<link href="http://fonts.googleapis.com/css?family=Nunito:400,700,300" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+   <div id='wrapper'>
+   		<div id='poem_content'>
+   			<h1>Circles</h1>
+   			<h2>Interactive Poem</h2>
+   			<p>
+   			One two three<br />
+   			Peter, Paul, and Mary<br />
+   			Wait, no stop that<br />
+   			Not Cool<br />
+   			</p>
+   			<p>
+   			Let's talk<br />
+   			Let's have<br />
+   			A party<br />
+   			In a communal pair of pants<br />
+   			</p>
+   		</div>
+   		<div id='main_content'>
+   			<div id='canvas'> </div>
+   			<div id='picker'>
+   				<h1>It's your turn:</h1>
+   				Who do you choose? Potato.
+   			</div>
+   		</div>
+	</div>
+   	</body>
+</html>
