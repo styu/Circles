302c7aa2df1ee609e93f1302e23b526442db1a92
Victor Hung <victorhung92@gmail.com>
2011-12-13 03:36:53 -0500

    word wrap

--- php/choose.php
+++ php/choose.php
@@ -3,12 +3,13 @@ require_once('db_setup.php');
 $link = db_default_connection();
 db_setup_connections_table($link);
 echo "<div id = 'picker'>
+<h1>It's your turn:</h1>
 ";
 if (!empty($_POST['promptID'])){
 	$sql = mysql_query("SELECT * FROM prompts WHERE id = '" . $_POST['promptID'] . "'");
 	$data = mysql_fetch_array($sql);
 	if (!$data){
-		echo "<h1>Herp derp a cow, records in your SQL database are missing</h1>";
+		echo "<h2>Herp derp a cow, records in your SQL database are missing</h2>";
 	}
 	else{
 		if ($data['nextPrompt'] == '999999'){
@@ -16,7 +17,7 @@ if (!empty($_POST['promptID'])){
 			if ($result[0] == '(ALONE)' || $result[0] == '(END)'){
 				$text = substr($data['text'], strlen($result[0])+1);
 				echo "<h1>$text</h1>
-				<ul id = '999999'>
+				<ul>
 					<li><a href = '#' id = '$data[choice1]'>Retry</a></li>
 				</ul>
 				";
@@ -24,18 +25,16 @@ if (!empty($_POST['promptID'])){
 			else{
 				if ($result[1] == '(ORANGE)'){
 					$text = substr($data['text'], strlen($result[0])+strlen($result[1])+1);
-					$index = getColor($result[0]);
-					echo "<h1>$text</h1>
-					<ul id = '000001' name = '$index'>
+					echo "<h2>$text</h2>
+					<ul>
 						<li><a href = '#' id = '000001'>Play Again</a></li>
 					</ul>
 					";
 				}
 				else{
 					$text = substr($data['text'], strlen($result[0])+1);
-					$index = getColor($result[0]);
-					echo "<h1>$text</h1>
-					<ul id = '000001' name = '$index'>
+					echo "<h2>$text</h2>
+					<ul>
 						<li><a href = '#' id = '000001'>Play Again</a></li>
 					</ul>
 					";
@@ -43,14 +42,14 @@ if (!empty($_POST['promptID'])){
 			}
 		}
 		else if ($data['nextPrompt'] != '000000'){
-			echo "<h1>$data[text]</h1>
+			echo "<h2>$data[text]</h2>
 			<ul id = '$data[id]'>
 				<li><a href='#' id = '" . $data['nextPrompt'] . "'>Continue</a></li>
 			</ul>
 			";
 		}
 		else{
-			$sql1 = mysql_query("SELECT * FROM choices WHERE id = '" . $data['choice1'] . "'");
+			$sql1 = mysql_query("SELECT promptID, text FROM choices WHERE id = '" . $data['choice1'] . "'");
 			$choice1 = mysql_fetch_array($sql1);
 			if (!$choice1){
 				$choice1_text = '';
@@ -59,10 +58,8 @@ if (!empty($_POST['promptID'])){
 			else{
 				$choice1_text = $choice1['text'];
 				$choice1_id = $choice1['promptID'];
-				$choice1_char = $choice1['character'];
-				$choice1_param = $choice1['distance'] . ' ' . $choice1['size'];
 			}
-			$sql2 = mysql_query("SELECT * FROM choices WHERE id = '" . $data['choice2'] . "'");
+			$sql2 = mysql_query("SELECT promptID, text FROM choices WHERE id = '" . $data['choice2'] . "'");
 			$choice2 = mysql_fetch_array($sql2);
 			if (!$choice2){
 				$choice2_text = '';
@@ -71,10 +68,8 @@ if (!empty($_POST['promptID'])){
 			else{
 				$choice2_text = $choice2['text'];
 				$choice2_id = $choice2['promptID'];
-				$choice2_char = $choice2['character'];
-				$choice2_param = $choice2['distance'] . ' ' . $choice2['size'];
 			}
-			$sql3 = mysql_query("SELECT * FROM choices WHERE id = '" . $data['choice3'] . "'");
+			$sql3 = mysql_query("SELECT promptID, text FROM choices WHERE id = '" . $data['choice3'] . "'");
 			$choice3 = mysql_fetch_array($sql3);
 			if (!$choice3){
 				$choice3_text = '';
@@ -83,22 +78,20 @@ if (!empty($_POST['promptID'])){
 			else{
 				$choice3_text = $choice3['text'];
 				$choice3_id = $choice3['promptID'];
-				$choice3_char = $choice3['character'];
-				$choice3_param = $choice3['distance'] . ' ' . $choice3['size'];
 			}
-			echo "<h1>$data[text]</h1>
-			<ul id = '$data[id]'>
+			echo "<h2>$data[text]</h2>
+			<ul>
 			";
 				if ($choice1_text != ''){
-					echo "<li><a href = '#' id = '$choice1_id' name = '$choice1_char' rel = '$choice1_param'>$choice1_text</a></li>
+					echo "<li><a href = '#' id = '$choice1_id'>$choice1_text</a></li>
 					";
 				}
 				if ($choice2_text != ''){
-					echo "<li><a href = '#' id = '$choice2_id' name = '$choice2_char' rel = '$choice2_param'>$choice2_text</a></li>
+					echo "<li><a href = '#' id = '$choice2_id'>$choice2_text</a></li>
 					";
 				}
 				if ($choice3_text != ''){
-					echo "<li><a href = '#' id = '$choice3_id' name = '$choice3_char' rel = '$choice3_param'>$choice3_text</a></li>
+					echo "<li><a href = '#' id = '$choice3_id'>$choice3_text</a></li>
 					";
 				}
 			echo "</ul>
@@ -111,26 +104,4 @@ if (!empty($_POST['promptID'])){
 	}
 }
 echo "</div>";
-
-function getColor($color){
-	if ($color == '(BLACK)'){
-		return 1;
-	}
-	else if ($color == '(BLUE)'){
-		return 2;
-	}
-	else if ($color == '(GREEN)'){
-		return 3;
-	}
-	else if ($color == '(RED)'){
-		return 4;
-	}
-	else if ($color == '(ORANGE)'){
-		return 5;
-	}
-	else if ($color == '(PINK)'){
-		return 6;
-	}
-}
-mysql_close($link);
 ?>
