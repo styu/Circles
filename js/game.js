191230c59fffb2c7a175c316cf5e4c0b2c1b3750
Steph <styu314@gmail.com>
2011-12-13 03:09:42 -0500

    moar effectz

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	var count = 1;
 	var left_shift = [-1, 1, -1, 1, -1, 1];
 	var top_shift = [0, 0, 1, 1, -1, -1];
+	//saved to retry
+	var retry = false;
 	var left = 10;
 	var top = 17;
 	//initial game state
@@ -22,6 +24,16 @@ $(document).ready(function(){
 		var character = $(this).attr('name');
 		var attributes = $(this).attr('rel').split(" ");
 		//moving the circles
+		if (previous == '999999'){
+			$('.c0').remove();
+			$("<div class='circle c0'></div>").appendTo($('#canvas')).fadeIn();
+			$('.c1').fadeIn();
+			$('.c2').fadeIn();
+			$('.c3').fadeIn();
+			$('.c4').fadeIn();
+			$('.c5').fadeIn();
+			$('.c6').fadeIn();
+		}
 		if (character != 0){
 			var circle = ".c" + character;
 			var orig_left = $(".c0").css('left');
@@ -66,11 +78,22 @@ $(document).ready(function(){
 			var div = $(data);
 			$(data).appendTo('#main_content').fadeIn();
 			var poem = unescape($('#poem_hidden').text());
-			if (previous == '000001'){
+			if (previous == '000001' && retry){
 				$('#poem').empty();
 				$('#poem').append("<p id = '1'>"+poem+"<br /></p>").fadeIn();
+				$('#canvas').hide().empty();
+				$('#canvas').append("<div class='circle c0'></div>")
+							.append("<div class='circle c1'></div>")
+							.append("<div class='circle c2'></div>")
+							.append("<div class='circle c3'></div>")
+							.append("<div class='circle c4'></div>")
+							.append("<div class='circle c5'></div>")
+							.append("<div class='circle c6'></div>")	
+							.fadeIn(1000);
+
 				init = 1;
 				count = 1;
+				retry = false;
 			}
 			else{
 				if (count < 4){
@@ -84,6 +107,81 @@ $(document).ready(function(){
 				}
 			}
 			$('#poem_hidden').remove();
+			if ($('#picker ul').attr('id') == '999999'){
+				$('.c1').fadeOut(500);
+				$('.c2').fadeOut(500);
+				$('.c3').fadeOut(500);
+				$('.c4').fadeOut(500);
+				$('.c5').fadeOut(500);
+				$('.c6').fadeOut(500);
+				$('.c0').animate({
+					left:"180px",
+					top: "100px",
+					height: "180px",
+					width: "180px",
+					borderTopLeftRadius: "180px", 
+				    borderTopRightRadius: "180px", 
+				    borderBottomLeftRadius: "180px", 
+				    borderBottomRightRadius: "180px",
+				    WebkitBorderTopLeftRadius: "180px", 
+				    WebkitBorderTopRightRadius: "180px", 
+				    WebkitBorderBottomLeftRadius: "180px", 
+				    WebkitBorderBottomRightRadius: "180px", 
+				    MozBorderRadius: "180px" 
+				}, 750);
+			}
+			if ($('#picker ul').attr('name') > 0){
+				retry = true;
+				var friend = $('#picker ul').attr('name');
+				if (friend != 1){
+					$('.c1').fadeOut(500);
+				}
+				if (friend != 2){
+					$('.c2').fadeOut(500);
+				}
+				if (friend != 3){
+					$('.c3').fadeOut(500);
+				}
+				if (friend != 4){
+					$('.c4').fadeOut(500);
+				}
+				if (friend != 5){
+					$('.c5').fadeOut(500);
+				}
+				if (friend != 6){
+					$('.c6').fadeOut(500);
+				}
+				$('.c0').animate({
+					left:"80px",
+					top: "100px",
+					height: "180px",
+					width: "180px",
+					borderTopLeftRadius: "180px", 
+				    borderTopRightRadius: "180px", 
+				    borderBottomLeftRadius: "180px", 
+				    borderBottomRightRadius: "180px",
+				    WebkitBorderTopLeftRadius: "180px", 
+				    WebkitBorderTopRightRadius: "180px", 
+				    WebkitBorderBottomLeftRadius: "180px", 
+				    WebkitBorderBottomRightRadius: "180px", 
+				    MozBorderRadius: "180px" 
+				}, 750);
+				$('.c' + friend).animate({
+					left: "300px",
+					top: "100px",
+					height: "180px",
+					width: "180px",
+					borderTopLeftRadius: "180px", 
+				    borderTopRightRadius: "180px", 
+				    borderBottomLeftRadius: "180px", 
+				    borderBottomRightRadius: "180px",
+				    WebkitBorderTopLeftRadius: "180px", 
+				    WebkitBorderTopRightRadius: "180px", 
+				    WebkitBorderBottomLeftRadius: "180px", 
+				    WebkitBorderBottomRightRadius: "180px", 
+				    MozBorderRadius: "180px" 
+				}, 750); 
+			}
 		});
 		
 	});
