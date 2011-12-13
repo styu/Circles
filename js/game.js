4cc9c3e10b64c861d71c87f74d0c07e6a5539654
Steph <styu314@gmail.com>
2011-12-13 02:05:56 -0500

    circles move now.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 	var init = 1;
 	var count = 1;
+	var left_shift = [-1, 1, -1, 1, -1, 1];
+	var top_shift = [0, 0, 1, 1, -1, -1];
+	var left = 10;
+	var top = 17;
 	//initial game state
 	id = getID(init, 6);
 	$.post('php/choose.php', {promptID: id}, function(data) {
@@ -15,6 +19,48 @@ $(document).ready(function(){
 	$('li a').live('click', function(){
 		var id = $(this).attr('id');
 		var previous = $('#picker ul').attr('id');
+		var character = $(this).attr('name');
+		var attributes = $(this).attr('rel').split(" ");
+		//moving the circles
+		if (character != 0){
+			var circle = ".c" + character;
+			var orig_left = $(".c0").css('left');
+			var orig_top = $(".c0").css('top');
+			var circle_left = $(circle).css('left');
+			var circle_top = $(circle).css('top');
+			var newLeft = parseInt(attributes[0])*left_shift[character-1]*left;
+			if (Math.abs(parseInt(circle_left.substring(0, circle_left.length-2))+newLeft-parseInt(orig_left.substring(0, orig_left.length-2))) <= 46){
+				newLeft = 0;
+				newTop = 0;
+			}
+			var newTop = parseInt(attributes[0])*top_shift[character-1]*top;
+			if (Math.abs(parseInt(circle_top.substring(0, circle_top.length-2)) + newTop - parseInt(orig_top.substring(0, orig_top.length-2))) <= 46){
+				newTop = 0;
+			}
+
+			var radiusChange = 1 + 0.2*parseInt(attributes[1]);
+			var circle_radius = $(circle).css('height');
+			var new_radius = Math.round(parseInt(circle_radius.substring(0, circle_radius.length-2))*radiusChange);
+			if (new_radius <= 10){
+				new_radius = 10;
+			}
+			$(circle).animate({
+				left: '+='+newLeft,
+				top: '+='+newTop,
+				height: new_radius+'px',
+				width: new_radius+'px',
+				borderTopLeftRadius: new_radius, 
+			    borderTopRightRadius: new_radius, 
+			    borderBottomLeftRadius: new_radius, 
+			    borderBottomRightRadius: new_radius,
+			    WebkitBorderTopLeftRadius: new_radius, 
+			    WebkitBorderTopRightRadius: new_radius, 
+			    WebkitBorderBottomLeftRadius: new_radius, 
+			    WebkitBorderBottomRightRadius: new_radius, 
+			    MozBorderRadius: new_radius 
+			}, 500);
+		}
+		//updating to next prompt and set of choices
 		$.post('php/choose.php', {promptID:id}, function(data) {
 			$('#picker').fadeOut().remove();
 			var div = $(data);
